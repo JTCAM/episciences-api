52b09149618534d6673df8ccac2f071eaf9fe63a
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-12-08 10:30:02 +0100

    token management

--- episcience-api.py
+++ episcience-api.py
@@ -8,10 +8,11 @@ url = 'https://api.episciences.org'
 
 
 def fetch_token():
+    username = getpass.getuser()
     password = getpass.getpass()
     r = requests.post(
         url+'/api/login',
-        data=json.dumps({'username': 'ganciaux',
+        data=json.dumps({'username': username,
                          'password': password,
                          'code': 'jtcam'
                          }),
@@ -19,18 +20,25 @@ def fetch_token():
             "accept": "application/json",
             "Content-Type": "application/json",
         })
+    # print(r.content)
     r = r.json()
     return r
 
 
 def authenticate():
+    r = None
     if os.path.exists('token.json'):
-        f = open('token.json')
-        r = json.load(f)
-    else:
+        try:
+            f = open('token.json')
+            r = json.load(f)
+        except json.JSONDecodeError:
+            pass
+    elif r is None:
         r = fetch_token()
         with open('token.json', 'w') as f:
-            f.write(r)
+            f.write(json.dumps(r))
+    else:
+        raise RuntimeError("Error fetching the token")
     return r
 
 
