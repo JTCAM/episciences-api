16cd3cdbb47dd1e9672bb8ba5104fe1ba8cf4650
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2024-02-01 08:19:11 +0100

    clean prototype

--- episcience-api.py
+++ episcience-api.py
@@ -61,50 +61,55 @@ def epi_get_single(req, token, page=None):
             continue
         if code != 200:
             print(code)
-            print(r.content)
+            # print(r.content)
             return []
         r = r.json()
-        print('aaaa', code, r)
-        # print(r.keys())
         if 'hydra:member' in r:
-            print(r['hydra:totalItems'])
+            print('hydra:totalItems: ', r['hydra:totalItems'])
             # print(r['hydra:search'])
-            return r['hydra:member']
-        
+            ret = r['hydra:member']
+            return ret
+
         return r
+
+################################################################
 
 
 def epi_get(req, token):
-    page = 1
-    ret = epi_get_single(req, token, page)
-    if ret is None:
-        return []
     r = []
+    ret = 1
+    page = 0
     while ret:
         page += 1
         ret = epi_get_single(req, token, page)
         print('returned len', len(ret))
         for p in ret:
             print(p['@id'])
-        # print(ret)
         r += ret
-        break
     return r
+
+################################################################
 
 
 def list_papers(token):
     r = epi_get('/api/papers', token)
     return r
 
+################################################################
+
 
 def list_users(token):
     r = epi_get('/api/users', token)
     return r
 
+################################################################
+
 
 def get_user(uid, token):
     r = epi_get_single(f'/api/users/{uid}', token)
     return r
+
+################################################################
 
 
 def get_paper(uid, token):
@@ -112,12 +117,19 @@ def get_paper(uid, token):
     return r
 
 
+################################################################
 token = authenticate()
-print(token['token'])
-print("papers")
+# print(token['token'])
+print('*'*60)
+print("Fetch papers")
+print('*'*60)
 
 papers = list_papers(token)
 print(f'Found {len(papers)} papers')
+
+print('*'*60)
+print("Found papers")
+print('*'*60)
 for p in papers:
     print('\n' + '*'*60 + '\n')
     for k, v in p.items():
@@ -131,8 +143,6 @@ for p in papers:
 #     for k, v in p.items():
 #         print(k, v)
 
-print('*'*30)
-print('*'*30)
 # u = get_paper(11497, token)
 # for k, v in u.items():
 #     print(k, v)
