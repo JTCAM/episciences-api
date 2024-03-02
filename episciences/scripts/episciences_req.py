1fe7d59324c83cadaaf8744b6efcab6707c08ef9
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2024-03-02 12:12:19 +0100

    adding scripts

--- episciences/scripts/episciences_req.py
+++ episciences/scripts/episciences_req.py
@@ -4,24 +4,29 @@ import episciences_api as epi
 import argparse
 ################################################################
 
-parser = argparse.ArgumentParser()
-parser.add_argument("paper", type=str,
-                    help="Provide the paper to extract information from")
-args = parser.parse_args()
 
-conn = epi.EpiscienceDB()
-p = conn.get_paper(args.paper)
-with open('paper.json', 'w') as f:
-    f.write(json.dumps(p.json))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("paper", type=str,
+                        help="Provide the paper to extract information from")
+    args = parser.parse_args()
+
+    conn = epi.EpiscienceDB()
+    p = conn.get_paper(args.paper)
+    with open('paper.json', 'w') as f:
+        f.write(json.dumps(p.json))
+
+    print('Title:', p.title['#text'])
+    print('Authors:', '; '.join(p.creator))
+    print('Status:', p.status)
+    print('submissionDate:', p.submissionDate)
+    print('Date:', p.date)
+    print('Description:', p.description['#text'])
+    print('Identifiers:', ', '.join(p.identifier))
+    print('Available_fields:', dir(p))
+    json_formatted_str = json.dumps(p.json, indent=2)
+    print('Full_content:\n', json_formatted_str)
 
 
-print('Title:', p.title['#text'])
-print('Authors:', '; '.join(p.creator))
-print('Status:', p.status)
-print('submissionDate:', p.submissionDate)
-print('Date:', p.date)
-print('Description:', p.description['#text'])   
-print('Identifiers:', ', '.join(p.identifier))
-print('Available_fields:', dir(p))
-json_formatted_str = json.dumps(p.json, indent=2)
-print('Full_content:\n', json_formatted_str)
+if __name__ == "__main__":
+    main()
